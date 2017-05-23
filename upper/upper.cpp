c5b4b84cdb562641399a156f913cf57d72ba24ba
David Zemon <david@zemon.name>
2017-05-22 20:31:41 -0500

    Add some trace logging and a useless for-loop

--- upper/upper.cpp
+++ upper/upper.cpp
@@ -1,4 +1,7 @@
 #include <sstream>
+#if TRACE == 1
+#include <iostream>
+#endif
 
 #include <upper.h>
 
@@ -8,6 +11,9 @@ std::string to_upper(const std::string &input) {
   std::stringstream ss;
   for (const auto item : input)
     ss << static_cast<char>(std::toupper(item));
+#if TRACE == 1
+  std::cout << "[Upper] Finished for-loop" << std::endl;
+#endif
   return ss.str();
 }
 
